b0ade6d8c13b52a11000cbc962a2c87d7a117bb1
DrAlienTech <alientechgamig@hotmail.com>
2022-07-24 21:41:46 -0400

    index qna and styling

--- sfscioly/scripts/dashboard.js
+++ sfscioly/scripts/dashboard.js
@@ -9,18 +9,36 @@ import {
     getDocs,
 } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js';
 
-export function loadBulletinBoard(visibility) {
-    getDocs(collection(db, "dashboard", visibility, "bulletinboard")).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            document.getElementById("bulletinboardPublicItemContainer").innerHTML += `
-            <div id="${doc.id}" class="bulletin-board-item">
-                <h1>${doc.data().title}</h1>
+import {
+    getDatabase,
+    ref,
+    child,
+    get,
+} from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js';
 
-                <div>
-                    ${doc.data().text}
-                </div>
-            </div>
-        `;
-        })
+const rtdb = getDatabase();
+
+export function loadBulletinBoard() {
+    get(child(ref(rtdb), `bulletinboard`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            window.bulletinboard = Object.entries(snapshot.val());
+
+            console.log(bulletinboard)
+
+            bulletinboard.forEach((post) => {
+                document.getElementById("bulletinboardItemContainer").innerHTML += `
+                    <div id="${post[0] + Math.random()}" class="bulletin-board-item">
+                        <h1>${post[1]["title"]}</h1>
+                        <div>
+                            ${post[1]["content"]}
+                        </div>
+                    </div>
+                `;
+            });
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
     });
 }
